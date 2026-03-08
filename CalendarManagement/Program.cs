61dd6f5347ab974b7c4868c6074961ce801e0cfe
jouhakie-boop <jouhakie@gmail.com>
2026-03-08 17:15:12 +0800

    Update Program.cs

--- CalendarManagement/Program.cs
+++ CalendarManagement/Program.cs
@@ -6,8 +6,9 @@ namespace CalendarManagement
     internal class Program
 
     {
-        static List<string> reminders = new List<string>();
-        static List<string> events = new List<string>();
+        static Dictionary<string, List<string>> reminders = new Dictionary<string, List<string>>();
+        static Dictionary<string, List<string>> events = new Dictionary<string, List<string>>();
+
 
 
         static void DisplayMenu()
@@ -52,65 +53,119 @@ namespace CalendarManagement
             }
         }
 
-            static void CreateReminder()
-            {
-                Console.Write("Enter the Name or Title of the Reminder: ");
-                string RemindersName = Console.ReadLine();
-                Console.Write("Enter Date (February 10, 2026): ");
-                string Reminderdate = Console.ReadLine();
-                Console.Write("Enter Day (Tuesday): ");
-                string Reminderday = Console.ReadLine();
-                Console.Write("Enter Time (9 PM/9 AM): ");
-                string Remindertime = Console.ReadLine();
-                
-                reminders.AddRange(new List<string> { RemindersName, Reminderdate, Reminderday, Remindertime });
-                Console.WriteLine("Reminder Created Successfully!");
-            }
-
-            static void CreateEvent()
-            {
-                Console.Write("Enter the Name or Title of the Event: ");
-                string eventsName = Console.ReadLine();
-                Console.Write("Enter Date (February 10, 2026): ");
-                string Eventdate = Console.ReadLine();
-                Console.Write("Enter Day (Tuesday): ");
-                string Eventday = Console.ReadLine();
-                Console.Write("Enter Time (9:00PM): ");
-                string Eventtime = Console.ReadLine();
-             
-                events.AddRange(new List<string> { eventsName, Eventdate, Eventday, Eventtime });
-                Console.WriteLine("Event Created Successfully!");
-            }
-
-            static void ViewReminders()
-            {
-                var reminders = new List<string>();
-                foreach (var reminder in reminders)
-                {
-                    Console.WriteLine(reminder);
-                }
-            }
-
-            static void ViewEvents()
-            {
-                var events = new List<string>();
-                foreach (var ev in events)
-                {
-                    Console.WriteLine(ev);
-                }
-            }
-
-        static void UpdateReminder()
+        static void CreateReminder()
         {
-            Console.Write("Would you like to update a reminder? (yes/no)");
-            string updateChoice = Console.ReadLine();
-            string choice = updateChoice.ToLower();
+            Console.Write("Enter the Name or Title of the Reminder: ");
+            string RemindersName = Console.ReadLine();
+            Console.Write("Enter Date (February 10, 2026): ");
+            string Reminderdate = Console.ReadLine();
+            Console.Write("Enter Day (Tuesday): ");
+            string Reminderday = Console.ReadLine();
+            Console.Write("Enter Time (9 PM/9 AM): ");
+            string Remindertime = Console.ReadLine();
 
-            if (choice == "yes")
+            //   reminders.AddRange(new List<string> { RemindersName, Reminderdate, Reminderday, Remindertime });
+            reminders[RemindersName] = new List<string> { Reminderdate, Reminderday, Remindertime };
+            Console.WriteLine("Reminder Created Successfully!");
+        }
+
+        static void CreateEvent()
+        {
+            Console.Write("Enter the Name or Title of the Event: ");
+            string eventsName = Console.ReadLine();
+            Console.Write("Enter Date (February 10, 2026): ");
+            string Eventdate = Console.ReadLine();
+            Console.Write("Enter Day (Tuesday): ");
+            string Eventday = Console.ReadLine();
+            Console.Write("Enter Time (9:00PM): ");
+            string Eventtime = Console.ReadLine();
+
+            //   events.AddRange(new List<string> { eventsName, Eventdate, Eventday, Eventtime });
+            events[eventsName] = new List<string> { Eventdate, Eventday, Eventtime };
+            Console.WriteLine("Event Created Successfully!");
+        }
+
+        static void ViewReminders()
+        {
+            Console.Write("Enter Reminder Name to view: ");
+            string reminderToView = Console.ReadLine();
+
+            if (reminders.ContainsKey(reminderToView))
             {
-
+                Console.WriteLine($"{reminderToView}:");
+                foreach (var reminderdetail in reminders[reminderToView])
+                {
+                    Console.WriteLine(reminderdetail);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Reminder not found.");
             }
         }
+
+        static void ViewEvents()
+        {
+            Console.Write("Enter Event Name to view: ");
+            string eventToView = Console.ReadLine();
+
+            if (reminders.ContainsKey(eventToView))
+            {
+                Console.WriteLine($"{eventToView}:");
+                foreach (var eventdetail in reminders[eventToView])
+                {
+                    string eventdetails = eventdetail.ToUpper();
+                    Console.WriteLine(eventdetail);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Reminder not found");
+            }
+
+            static void UpdateReminder()
+            {
+                Console.Write("Would you like to update a reminder? (yes/no)");
+                string updateChoice = Console.ReadLine();
+                string choice = updateChoice.ToLower();
+
+                if (choice == "yes")
+                {
+                    Console.Write("Enter the Name of the Reminder you want to update: ");
+                    string reminderNametoUpdate = Console.ReadLine();
+
+
+
+                }
+                else if (choice == "no")
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    //  Console.WriteLine("Would you like to try again?");
+                }
+            }
+
+            static void UpdateEvent()
+            {
+                Console.Write("Would you like to update a reminder? (yes/no)");
+                string updateChoice = Console.ReadLine();
+                string choice = updateChoice.ToLower();
+
+                if (choice == "yes")
+                {
+
+                }
+                else if (choice == "no")
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    //  Console.WriteLine("Would you like to try again?");
+                }
+            }
 
             static void Main(string[] args)
             {
@@ -126,6 +181,7 @@ namespace CalendarManagement
             }
         }
     }
+}
 
    
 
